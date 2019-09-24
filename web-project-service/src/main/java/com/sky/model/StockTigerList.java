b32c65b1e8cf1363ce5ed1570db6756a685404a0
lxl <1574582016@qq.com>
2019-09-24 22:23:05 +0800

    统计

--- web-project-service/src/main/java/com/sky/model/StockTigerList.java
+++ web-project-service/src/main/java/com/sky/model/StockTigerList.java
@@ -1,8 +1,11 @@
 package com.sky.model;
 
+import com.baomidou.mybatisplus.annotations.TableField;
 import com.baomidou.mybatisplus.annotations.TableName;
 import com.sky.core.model.BaseModel;
 import lombok.Data;
+
+import java.math.BigDecimal;
 
 /**
  * Created by ThinkPad on 2019/9/24.
@@ -10,4 +13,70 @@ import lombok.Data;
 @Data
 @TableName("stock_tiger_list")
 public class StockTigerList extends BaseModel<StockTigerList> {
+
+    /**
+     * 股票编码
+     */
+    @TableField("stock_code")
+    private String stockCode ;
+
+    /**
+     * 股票名称
+     */
+    @TableField("stock_name")
+    private String stockName ;
+
+
+    /**
+     * 发布日期
+     */
+    @TableField("publish_time")
+    private String publishTime ;
+
+
+    /**
+     * 涨跌幅
+     */
+    @TableField("upper_range")
+    private BigDecimal upperRange ;
+
+    /**
+     *换手率
+     */
+    @TableField("hand_rate")
+    private BigDecimal handRate ;
+
+
+    /**
+     *上榜原因
+     */
+    @TableField("focus_reason")
+    private String focusReason ;
+
+
+    /**
+     *买入额
+     */
+    @TableField("buy_money")
+    private BigDecimal buyMoney ;
+
+    /**
+     *卖出额
+     */
+    @TableField("sell_money")
+    private BigDecimal sellMoney ;
+
+    /**
+     * 异动原因
+     */
+    @TableField("unusual_reason")
+    private String unusualReason ;
+
+
+    /**
+     * 主力选择原因
+     */
+    @TableField("choose_reason")
+    private String chooseReason ;
+
 }
--- web-project-web/src/test/java/com/sky/Test01.java
+++ web-project-web/src/test/java/com/sky/Test01.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.sky.core.utils.CommonHttpUtil;
+import com.sky.model.StockTigerList;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,18 +19,24 @@ public class Test01 {
 
     @Test
     public void test(){
-        String url = "http://data.eastmoney.com/DataCenter_V3/stock2016/TradeDetail/pagesize=200,page=1,sortRule=-1,sortType=,startDate=2019-08-24,endDate=2019-09-24,gpfw=0,js=var%20data_tab_1.html?rt=26154751";
+        String url = "http://data.eastmoney.com/DataCenter_V3/stock2016/TradeDetail/pagesize=200,page=1,sortRule=-1,sortType=,startDate=2019-09-24,endDate=2019-09-24,gpfw=0,js=var%20data_tab_1.html?rt=26154751";
         String jsStr = CommonHttpUtil.sendPost(url);
-        System.out.println(jsStr);
+//        System.out.println(jsStr);
         jsStr = jsStr.substring(jsStr.indexOf("data_tab_1=") + 11 , jsStr.length());
-        System.out.println(jsStr);
+//        System.out.println(jsStr);
         JSONArray jsonArray = JSON.parseObject(jsStr).getJSONArray("data");
         for(int i = 0 ; i < jsonArray.size() ; i ++){
            JSONObject jsonObject = jsonArray.getJSONObject(i);
-            System.out.println(jsonObject.toString());
+            StockTigerList tigerList = new StockTigerList();
+            tigerList.setStockCode(jsonObject.getString("SCode"));
+            tigerList.setStockName(jsonObject.getString("SName"));
+            tigerList.setPublishTime(jsonObject.getString("Tdate"));
+            tigerList.setUpperRange(jsonObject.getBigDecimal("Chgradio"));
+            tigerList.setHandRate(jsonObject.getBigDecimal("Dchratio"));
+            tigerList.setFocusReason(jsonObject.getString("Ctypedes"));
+            tigerList.setBuyMoney(jsonObject.getBigDecimal("Bmoney"));
+            tigerList.setSellMoney(jsonObject.getBigDecimal("Smoney"));
+            System.out.println(tigerList.toString());
         }
-//        JSONObject jsonObject = JSON.parseObject(jsStr);
-//        JSONObject dataObject = jsonObject.getJSONObject("data");
-//        System.out.println(dataObject);
     }
 }
