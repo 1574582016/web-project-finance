96790daad951deed4f90898a2a727ba706679e2c
liuxiaolong <taiyang@bainuo.com>
2019-09-29 18:17:17 +0800

    统计

---
+++ web-project-web/src/test/java/com/sky/Test02.java
@@ -0,0 +1,34 @@
+package com.sky;
+
+/**
+ * Created by ThinkPad on 2019/9/29.
+ */
+public class Test02 {
+
+/**
+ * 开会——议程
+ *     ——企业问题
+ *
+ *JOAN: Okay,Let's get started.Do you all have the agenda?
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+  */
+}
