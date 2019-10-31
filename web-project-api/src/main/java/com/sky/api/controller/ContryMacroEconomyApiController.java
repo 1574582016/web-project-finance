7983bf50691a60d14ba23bca37a915c4518eae0d
liuxiaolong <taiyang@bainuo.com>
2019-10-31 18:22:13 +0800

    统计

--- web-project-api/src/main/java/com/sky/api/controller/ContryMacroEconomyApiController.java
+++ web-project-api/src/main/java/com/sky/api/controller/ContryMacroEconomyApiController.java
@@ -25,22 +25,28 @@ public class ContryMacroEconomyApiController extends AbstractController {
 
     @LogRecord(name = "getContryMacroEconomy" ,description = "查询国际经济数据")
     @PostMapping("/getContryMacroEconomy")
-    public Object getContryMacroEconomy(String contry , String indexCode ,String startDay ,String endDay){
-        List<MacroEconomy_VO> list = contryMacroEconomyIndexService.getContryMacroEconomy( contry ,  indexCode , startDay , endDay);
-        List<String> title = new ArrayList<String>();
-        List<BigDecimal> arrayList = new ArrayList<BigDecimal>();
-        String name = "";
-        for(MacroEconomy_VO economy_vo : list){
-            if(StringUtils.isBlank(name)){
-                name = economy_vo.getClassName();
-            }
-            title.add(economy_vo.getPublishDay());
-            arrayList.add(economy_vo.getPublishValue());
-        }
+    public Object getContryMacroEconomy(String contry , String indexCodes ,String startDay ,String endDay){
         Map<String,Object> resultMap = new HashedMap();
+        List<String> title = new ArrayList<String>();
+        String[] codes = indexCodes.split(",");
+        for(String indexCode : codes){
+            String name = "";
+            List<BigDecimal> arrayList = new ArrayList<BigDecimal>();
+            List<MacroEconomy_VO> list = contryMacroEconomyIndexService.getContryMacroEconomy( contry ,  indexCode , startDay , endDay);
+            for(MacroEconomy_VO economy_vo : list){
+                if(StringUtils.isBlank(name)){
+                    name = economy_vo.getClassName();
+                    resultMap.put("name" + indexCode,name);
+                }
+                if(title.size() == 0){
+                    title.add(economy_vo.getPublishDay());
+                }
+                arrayList.add(economy_vo.getPublishValue());
+            }
+            resultMap.put("arrayList" + indexCode,arrayList);
+        }
+
         resultMap.put("title",title);
-        resultMap.put("name",name);
-        resultMap.put("arrayList",arrayList);
         return ResponseEntity.ok(MapSuccess("操作成功",resultMap));
     }
 }
