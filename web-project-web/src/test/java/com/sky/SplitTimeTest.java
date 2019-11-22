33ae81e33fe7860b0eb4ce6d1ca01eb141a5dbb5
liuxiaolong <taiyang@bainuo.com>
2019-11-22 18:21:14 +0800

    统计

--- web-project-web/src/test/java/com/sky/SplitTimeTest.java
+++ web-project-web/src/test/java/com/sky/SplitTimeTest.java
@@ -13,9 +13,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.*;
 
 /**
  * Created by ThinkPad on 2019/11/21.
@@ -94,6 +96,103 @@ public class SplitTimeTest {
         calendar.setTime(pointDate);
         int monthWeek = calendar.get(Calendar.WEEK_OF_MONTH);
         System.out.println(monthWeek);
+    }
+
+
+
+    @Test
+    public void test333() throws InterruptedException, ExecutionException {
+
+        // 开始时间
+        long start = System.currentTimeMillis();
+//        List<StockCode> list = stockCodeService.selectList(new EntityWrapper<StockCode>().where("stock_code NOT IN(SELECT stock_code FROM stock_deal_data)"));
+        List<StockCompanySector> list = stockCompanySectorService.selectList(new EntityWrapper<StockCompanySector>().where("LEFT(stock_code,2) != 68"));
+        // 每500条数据开启一条线程
+        int threadSize = 500;
+        // 总数据条数
+        int dataSize = list.size();
+        // 线程数
+        int threadNum = dataSize / threadSize + 1;
+        // 定义标记,过滤threadNum为整数
+        boolean special = dataSize % threadSize == 0;
+
+        // 创建一个线程池
+        ExecutorService exec = Executors.newFixedThreadPool(threadNum);
+        // 定义一个任务集合
+        List<Callable<Integer>> tasks = new ArrayList<Callable<Integer>>();
+        Callable<Integer> task = null;
+        List<StockCompanySector> cutList = null;
+
+        // 确定每条线程的数据
+        for (int i = 0; i < threadNum; i++) {
+            if (i == threadNum - 1) {
+                if (special) {
+                    break;
+                }
+                cutList = list.subList(threadSize * i, dataSize);
+            } else {
+                cutList = list.subList(threadSize * i, threadSize * (i + 1));
+            }
+            // System.out.println("第" + (i + 1) + "组：" + cutList.toString());
+            final List<StockCompanySector> listStr = cutList;
+            task = new Callable<Integer>() {
+
+                @Override
+                public Integer call() throws Exception {
+                    for(StockCompanySector stockCode : listStr){
+
+                        List<StockDealData> dataList = stockDealDataService.selectList(new EntityWrapper<StockDealData>().where("stock_code = {0} and deal_period = 1 " ,stockCode.getStockCode()));
+                        for(StockDealData dealData : dataList){
+                            String dealTime = dealData.getDealTime() ;
+                            JSONObject jsonObject = DateUtils.getYearMonthWeekDay(dealTime);
+                            dealData.setPointYear(jsonObject.getInteger("year"));
+                            dealData.setPointMonth(jsonObject.getInteger("month"));
+                            dealData.setPointWeek(jsonObject.getInteger("week"));
+                            dealData.setPointDay(jsonObject.getInteger("day"));
+                        }
+                        if(null!=dataList&&dataList.size()>0){
+                            int pointsDataLimit = 200;//限制条数
+                            Integer size = dataList.size();
+                            //判断是否有必要分批
+                            if(pointsDataLimit<size){
+                                int part = size/pointsDataLimit;//分批数
+                                System.out.println("共有 ： "+size+"条，！"+" 分为 ："+part+"批");
+                                for (int i = 0; i < part; i++) {
+                                    List<StockDealData> listPage = dataList.subList(0, pointsDataLimit);
+                                    stockDealDataService.updateBatchById(listPage);
+                                    //剔除
+                                    dataList.subList(0, pointsDataLimit).clear();
+                                }
+                                if(!dataList.isEmpty()){
+                                    stockDealDataService.updateBatchById(dataList);
+                                }
+                            }else{
+                                stockDealDataService.updateBatchById(dataList);
+                            }
+                        }else{
+                            System.out.println("没有数据!!!");
+                        }
+
+                        Thread.sleep(1000);
+
+                    }
+                    return 1;
+                }
+            };
+            // 这里提交的任务容器列表和返回的Future列表存在顺序对应的关系
+            tasks.add(task);
+        }
+
+        List<Future<Integer>> results = exec.invokeAll(tasks);
+
+        for (Future<Integer> future : results) {
+            System.out.println(future.get());
+        }
+
+        // 关闭线程池
+        exec.shutdown();
+        System.out.println("线程任务执行结束");
+        System.err.println("执行任务消耗了 ：" + (System.currentTimeMillis() - start) + "毫秒");
     }
 
 
