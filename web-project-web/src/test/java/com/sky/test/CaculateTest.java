d61b8c00e253b28367d03091d74cc78ba7a79a71
liuxiaolong <taiyang@bainuo.com>
2020-01-14 16:39:53 +0800

    统计

--- web-project-web/src/test/java/com/sky/test/CaculateTest.java
+++ web-project-web/src/test/java/com/sky/test/CaculateTest.java
@@ -1,22 +1,29 @@
 package com.sky.test;
 
+import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.sky.core.utils.ChartUtils;
 import com.sky.core.utils.DateUtils;
-import com.sky.model.MessagePriceStatic;
-import com.sky.model.StockCompanyNotice;
-import com.sky.model.StockCompanySector;
-import com.sky.model.StockDealData;
+import com.sky.core.utils.Serie;
+import com.sky.model.*;
 import com.sky.scheduler.StockCompanyNoticeScheduler;
 import com.sky.service.*;
 import com.sky.vo.CompanyProfit_VO;
+import com.sky.vo.StockCompanyProfitVO;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.block.BlockBorder;
+import org.jfree.data.category.DefaultCategoryDataset;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.awt.*;
 import java.math.BigDecimal;
 import java.util.*;
+import java.util.List;
 
 /**
  * Created by ThinkPad on 2019/9/17.
@@ -36,6 +43,9 @@ public class CaculateTest {
 
     @Autowired
     private StockCompanyProfitService stockCompanyProfitService ;
+
+    @Autowired
+    private StockMoneyFlowService stockMoneyFlowService ;
 
 
     @Test
@@ -277,6 +287,84 @@ public class CaculateTest {
         List<StockCompanySector> list = stockCompanySectorService.selectList(null);
         for(StockCompanySector sector : list){
             stockProfitIncreaseRateService.caculateStockProfitIncreaseRate(sector.getStockCode());
+        }
+    }
+
+
+    @Test
+    public void test009(){
+       List<StockMoneyFlow> list = stockMoneyFlowService.selectList(new EntityWrapper<StockMoneyFlow>().where("stock_code = '000333'").orderBy("deal_time DESC"));
+        List<JSONObject> jsonList = new ArrayList<>();
+       for(int i = 0 ; i < list.size() - 4 ; i ++){
+           StockMoneyFlow firstFlow = list.get(i);
+           StockMoneyFlow secondFlow = list.get(i+1);
+           StockMoneyFlow thirdFlow = list.get(i+2);
+           StockMoneyFlow forthFlow = list.get(i+3);
+           StockMoneyFlow fifthFlow = list.get(i+4);
+           JSONObject jsonObject = new JSONObject();
+           jsonObject.put("dealTime" , firstFlow.getDealTime());
+           jsonObject.put("currentPrice" , new BigDecimal(firstFlow.getDayPrice()).divide(BigDecimal.TEN , 2 , BigDecimal.ROUND_HALF_UP));
+           BigDecimal averagePrice = (new BigDecimal(firstFlow.getDayPrice()).add(new BigDecimal(secondFlow.getDayPrice())).add(new BigDecimal(thirdFlow.getDayPrice())).add(new BigDecimal(forthFlow.getDayPrice())).add(new BigDecimal(fifthFlow.getDayPrice()))).divide(BigDecimal.valueOf(5).multiply(BigDecimal.TEN) ,2 ,BigDecimal.ROUND_HALF_UP);
+           jsonObject.put("averagePrice" , averagePrice);
+
+           BigDecimal firstTotal = new BigDecimal(firstFlow.getMagorMoney()).add(new BigDecimal(firstFlow.getSuperMoney())).add(new BigDecimal(firstFlow.getBigMoney())).add(new BigDecimal(firstFlow.getMiddleMoney())).add(new BigDecimal(firstFlow.getSmallMoney()));
+           BigDecimal secondTotal = new BigDecimal(secondFlow.getMagorMoney()).add(new BigDecimal(secondFlow.getSuperMoney())).add(new BigDecimal(secondFlow.getBigMoney())).add(new BigDecimal(secondFlow.getMiddleMoney())).add(new BigDecimal(secondFlow.getSmallMoney()));
+           BigDecimal thirdTotal = new BigDecimal(thirdFlow.getMagorMoney()).add(new BigDecimal(thirdFlow.getSuperMoney())).add(new BigDecimal(thirdFlow.getBigMoney())).add(new BigDecimal(thirdFlow.getMiddleMoney())).add(new BigDecimal(thirdFlow.getSmallMoney()));
+           BigDecimal forthTotal = new BigDecimal(forthFlow.getMagorMoney()).add(new BigDecimal(forthFlow.getSuperMoney())).add(new BigDecimal(forthFlow.getBigMoney())).add(new BigDecimal(forthFlow.getMiddleMoney())).add(new BigDecimal(forthFlow.getSmallMoney()));
+           BigDecimal fifthTotal = new BigDecimal(fifthFlow.getMagorMoney()).add(new BigDecimal(fifthFlow.getSuperMoney())).add(new BigDecimal(fifthFlow.getBigMoney())).add(new BigDecimal(fifthFlow.getMiddleMoney())).add(new BigDecimal(fifthFlow.getSmallMoney()));
+
+           BigDecimal averageTotal = (firstTotal.add(secondTotal).add(thirdTotal).add(forthTotal).add(fifthTotal)).divide(BigDecimal.valueOf(5).multiply(BigDecimal.valueOf(100000000)) , 2 , BigDecimal.ROUND_HALF_UP);
+
+           jsonObject.put("averageTotal" , averageTotal);
+
+           jsonList.add(jsonObject);
+       }
+        jsonList.sort((o1, o2) -> o1.getString("dealTime").compareTo(o2.getString("dealTime")));
+        System.out.println(jsonList.toString());
+        createFlowImg("000333" , jsonList);
+    }
+
+    private void createFlowImg(String stockCode ,List<JSONObject> jsonList){
+        try {
+            if(jsonList.size() == 0 ){
+                return;
+            }
+            List<String> title = new ArrayList<>();
+            List<BigDecimal> currentPrice = new ArrayList<>();
+            List<BigDecimal> averagePrice = new ArrayList<>();
+            List<BigDecimal> averageTotal = new ArrayList<>();
+
+            for(JSONObject jsonObject : jsonList){
+                title.add(jsonObject.getString("dealTime"));
+                currentPrice.add(jsonObject.getBigDecimal("currentPrice"));
+                averagePrice.add(jsonObject.getBigDecimal("averagePrice"));
+                averageTotal.add(jsonObject.getBigDecimal("averageTotal"));
+            }
+            String[] categories = new String[title.size()];
+            title.toArray(categories);
+            Vector<Serie> series = new Vector<Serie>();
+            // 柱子名称：柱子所有的值集合
+            series.add(new Serie("当前价格", currentPrice));
+            series.add(new Serie("平均价格", averagePrice));
+            series.add(new Serie("平均流量", averageTotal));
+            // 1：创建数据集合
+            DefaultCategoryDataset dataset = ChartUtils.createDefaultCategoryDataset(series, categories);
+
+            // 2：创建Chart[创建不同图形]
+            JFreeChart chart = ChartFactory.createLineChart("利润增长", "", "收益（亿）", dataset);
+            // 3:设置抗锯齿，防止字体显示不清楚
+            ChartUtils.setAntiAlias(chart);// 抗锯齿
+            // 4:对柱子进行渲染[[采用不同渲染]]
+            ChartUtils.setLineRender(chart.getCategoryPlot(), false,true);//
+            // 5:对其他部分进行渲染
+            ChartUtils.setXAixs(chart.getCategoryPlot());// X坐标轴渲染
+            ChartUtils.setYAixs(chart.getCategoryPlot());// Y坐标轴渲染
+            // 设置标注无边框
+            chart.getLegend().setFrame(new BlockBorder(Color.WHITE));
+
+            ChartUtils.saveAsFile(chart , "E:/dataImg/"+ stockCode +"_profit.jpg" , 1024 , 420);
+        }catch (Exception e){
+            e.printStackTrace();
         }
     }
 }
